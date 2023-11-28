40723efe20aa7d133a33786e4888eab1d2217216
youssef <youssefkamal856@yahoo.com>
2023-11-28 17:57:43 +0200

    done

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if chr(letter) == 'q' or chr(letter) == 'e':
-        continue
-    else:
+    if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
