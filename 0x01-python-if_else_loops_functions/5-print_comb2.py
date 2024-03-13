cd391f75e3d3a8079a220e8955425cf05b59c9b0
youssef <youssefkamal856@yahoo.com>
2024-03-13 16:50:28 +0200

    done

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range(100):
+    if i < 10:
+        print("0{}".format(i), end=", " if i != 9 else "")
+    else:
+        print("{}".format(i), end=", " if i != 99 else "\n")
