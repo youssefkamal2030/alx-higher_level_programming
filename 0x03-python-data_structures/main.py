2893ac12d59c153ba293f2c876128445fcf9b89e
youssef <youssefkamal856@yahoo.com>
2024-03-16 11:23:53 +0200

    done

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-delete_at = __import__('11-delete_at').delete_at
+print_list_integer = __import__('0-print_list_integer').print_list_integer
 
 my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
+print_list_integer(my_list)
+
