cb0a69fb6391179db71511a35058d4a678e222bc
youssef <youssefkamal856@yahoo.com>
2024-03-21 13:16:09 +0200

    done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictonary is None:
-        return 'None'
+    if a_dictionary is None:
+        return None
     ans = 0
-    ans2 = 0
+    ans2 = None
     for key, value in a_dictionary.items():
         if value > ans:
             ans = value
             ans2 = key
-    if ans == 0:
-        return 'None'
-    else:
-        return ans2
+    return ans2
