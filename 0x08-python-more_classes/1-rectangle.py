33a80c78eff861bcaadd9802afb4b507174b2d16
youssef <youssefkamal856@yahoo.com>
2024-04-19 14:41:01 +0200

    done

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     def width(self):
         """Get/set the width of the rectangle."""
