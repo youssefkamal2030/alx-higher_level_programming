722d812550647a147c697f03b99864df1da8af85
youssef <youssefkamal856@yahoo.com>
2024-06-18 20:45:07 +0300

    done

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,9 @@ let x = parseInt(process.argv[2]);
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  while (x--) {
-    console.log('C is fun');
+  if (x > 0) {
+    while (x--) {
+      console.log('C is fun');
+    }
   }
 }
