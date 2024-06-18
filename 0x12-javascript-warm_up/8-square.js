8cbb1194b17861bb90a2a8bb38074dc2582061af
youssef <youssefkamal856@yahoo.com>
2024-06-18 20:50:58 +0300

    done

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,10 +3,12 @@ const x = parseInt(process.argv[2]);
 if (isNaN(x)) {
   console.log('Missing size');
 }
-for (let i = 0; i < x; i++) {
-  let line = '';
-  for (let j = 0; j < x; j++) {
-    line += 'x';
+if (x > 0) {
+  for (let i = 0; i < x; i++) {
+    let line = '';
+    for (let j = 0; j < x; j++) {
+      line += 'x';
+    }
+    console.log(line);
   }
-  console.log(line);
 }
