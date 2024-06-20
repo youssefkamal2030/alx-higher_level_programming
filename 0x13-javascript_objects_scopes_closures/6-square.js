f2c8d495c37b99c19276acb3f7a330fc16ae2441
youssef <youssefkamal856@yahoo.com>
2024-06-20 22:01:09 +0300

    done

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        let line = '';
+        for (let j = 0; j < this.width; j++) {
+          line += 'C';
+        }
+        console.log(line);
+      }
+    } else {
+      this.print();
+    }
+  }
+};
