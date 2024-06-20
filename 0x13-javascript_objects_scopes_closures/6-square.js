e85d30e35f763e088fdab1697111101b7e729c0a
youssef <youssefkamal856@yahoo.com>
2024-06-20 22:14:20 +0300

    done

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
-    if (c !== undefined) {
-      for (let i = 0; i < this.height; i++) {
-        let line = '';
-        for (let j = 0; j < this.width; j++) {
-          line += 'C';
-        }
-        console.log(line);
-      }
-    } else {
+    if (c === undefined) {
       this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
 };
